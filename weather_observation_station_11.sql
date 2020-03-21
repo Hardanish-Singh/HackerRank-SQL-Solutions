de0a12a60422aa6c2c256b83a91bcab302a978b7
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-21 18:01:20 -0400

    Add files via upload

---
+++ weather_observation_station_11.sql
@@ -0,0 +1,39 @@
+/****************************************************************
+ *      HACKERRANK WEATHER OBSERVATION STATION 11 CHALLENGE     *
+ *                                                              *
+ * Problem Statement                                            *
+ * Query the list of CITY names from STATION that do not end    *
+ * with vowels or do not end with vowels. Your result cannot    *
+ * contain duplicates.                                          *                                        
+ *                                                              *
+ * Input: Below is the Data Structure of the table              *
+ *                                                              *
+ *                      STATION                                 * 
+ *          ______________________________                      *
+ *         | Field         | Type         |                     *
+ *          ______________________________                      *
+ *         | ID            | Number       |                     *
+ *          ______________________________                      *
+ *          ______________________________                      *
+ *         | City          | Varchar2(21) |                     *
+ *          ______________________________                      *
+ *          ______________________________                      *
+ *         | State         | Varchar2(2)  |                     *
+ *          ______________________________                      *
+ *          ______________________________                      *
+ *         | Lat_n         | Number       |                     *
+ *          ______________________________                      *
+ *          ______________________________                      *
+ *         | Long_w        | Number       |                     *
+ *          ______________________________                      * 
+ *                                                              * 
+ ***************************************************************/
+
+SELECT
+        DISTINCT(STATION.CITY)
+FROM
+        STATION
+WHERE
+        SUBSTRING(LOWER(STATION.CITY), 1, 1) NOT IN ('a','e','i','o','u') 
+    OR
+        SUBSTRING(LOWER(STATION.CITY), LENGTH(STATION.CITY), LENGTH(STATION.CITY)) NOT IN ('a','e','i','o','u') 
