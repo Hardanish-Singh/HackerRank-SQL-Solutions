4d3ddc9d88d1fecc9c6c9e9142d761e117ed21ca
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-21 12:34:45 -0400

    Add files via upload

---
+++ japanese_city_name.sql
@@ -0,0 +1,36 @@
+/****************************************************************
+ *         HACKERRANK JAPANESE CITIES NAME CHALLENGE            *
+ *                                                              *
+ * Problem Statement                                            *
+ * Query the names of all the Japanese cities in the CITY table *
+ * The COUNTRYCODE for Japan is JPN.                            *                                                                               
+ *                                                              *
+ * Input: Below is the Data Structure of the table              *
+ *                                                              *
+ *                      CITY                                    * 
+ *          ______________________________                      *
+ *         | Field         | Type         |                     *
+ *          ______________________________                      *
+ *         | ID            | Number       |                     *
+ *          ______________________________                      *
+ *          ______________________________                      *
+ *         | Name          | Varchar2(17) |                     *
+ *          ______________________________                      *
+ *          ______________________________                      *
+ *         | CountryCode   | Varchar2(3)  |                     *
+ *          ______________________________                      *
+ *          ______________________________                      *
+ *         | District      | Varchar2(20) |                     *
+ *          ______________________________                      *
+ *          ______________________________                      *
+ *         | Population    | Number       |                     *
+ *          ______________________________                      * 
+ *                                                              *    
+ ***************************************************************/
+
+SELECT
+        CITY.NAME
+FROM
+        CITY
+WHERE
+        CITY.COUNTRYCODE = 'JPN'
