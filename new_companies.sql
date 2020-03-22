499bd612cfaeaf7caa96465243d71883ae0ad3ac
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-22 18:16:36 -0400

    Update new_companies.sql

--- new_companies.sql
+++ new_companies.sql
@@ -2,31 +2,31 @@
  *              HACKERRANK NEW COMPANIES CHALLENGE              *
  *                                                              *
  * Problem Statement                                            *
- * Amber's conglomerate corporation just acquired some new 		*
+ * Amber's conglomerate corporation just acquired some new 	*
  * companies. Each of the companies follows this hierarchy: 	*
  *                                                              *
- * 						FOUNDER									*
- *  					   ↓                                    *
- * 						LEAD MANAGER                            *
- *						   ↓									*
- * 						SENIOR MANAGER							*
- *  					   ↓									*
- * 						MANAGER									*
- *						   ↓									*
- * 						EMPLOYEE								*
+ * 				FOUNDER				*
+ *  				   ↓                            *
+ * 				LEAD MANAGER                    *
+ *			           ↓				*
+ * 				SENIOR MANAGER			*
+ *  			           ↓				*
+ * 				MANAGER				*
+ *				   ↓				*
+ * 				EMPLOYEE			*
  *                                                              *
  * Given the table schemas below, write a query to print the 	*
  * company_code, founder name, total number of lead managers, 	*
  * total number of senior managers, total number of managers, & *
  * total number of employees. Order your output by ascending 	*
- * company_code.												*
+ * company_code.						*
  *                                                              *
- * Note:														*
- * The tables may contain duplicate records.					*
+ * Note:							*
+ * The tables may contain duplicate records.			*
  * The company_code is string, so the sorting should not be 	*
  * numeric. For example, if the company_codes are C_1, C_2, & 	*
  * C_10, then the ascending company_codes will be C_1, C_10, & 	*
- * & C_2.														*
+ * & C_2.							*
  *                                                              *   
  ***************************************************************/
 
