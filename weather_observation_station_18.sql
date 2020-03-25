30d62eafa0143ab3aaa856a41f9c5616b61e1025
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-25 19:50:49 -0400

    Update weather_observation_station_18.sql

--- weather_observation_station_18.sql
+++ weather_observation_station_18.sql
@@ -4,16 +4,16 @@
  * Problem Statement                                            *
  * Consider P1(a,b) & P2(c,d) to be two points on a 2D plane.   *
  * 1) a happens to equal the minimum value in Northern Latitude	* 
- * (LAT_N in STATION).											*
+ * (LAT_N in STATION).						*
  * 2) b happens to equal the minimum value in Western Longitude *
- * (LONG_W in STATION).											*
+ * (LONG_W in STATION).						*
  * 3) c happens to equal the maximum value in Northern Latitude	*
- * (LAT_N in STATION).											*
+ * (LAT_N in STATION).						*
  * 4) d happens to equal the maximum value in Western Longitude *
- * (LONG_W in STATION).											*
- *																*
+ * (LONG_W in STATION).						*
+ *								*								*
  * Query the Manhattan Distance between points P1(a,b) & P2(c,d)*  
- * & round it to a scale of 4 decimal places.					*
+ * & round it to a scale of 4 decimal places.			*
  *                                                              *
  * Input: Below is the Data Structure of the table              *
  *                                                              *
