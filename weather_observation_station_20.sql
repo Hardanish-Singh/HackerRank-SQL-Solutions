775803b8b2d4b0ca4661181ebc63ee9e3d534f29
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-28 17:57:19 -0400

    Update weather_observation_station_20.sql

--- weather_observation_station_20.sql
+++ weather_observation_station_20.sql
@@ -5,7 +5,7 @@
  * A median is defined as a number separating the higher half of*
  * a data set from the lower half. Query the median of the      *	
  * Northern Latitudes (LAT_N) from STATION and round your answer*
- * to 4 decimal places.					                        *		
+ * to 4 decimal places.					        *		
  *                                                              *
  * Input: Below is the Data Structure of the table              *
  *                                                              *
